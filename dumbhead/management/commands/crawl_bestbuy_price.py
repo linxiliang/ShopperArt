0b72cbaa82a82e53aae70ec8242f635477565a62
Hongyang Gao <hongyang.gao@wsu.edu>
2016-12-20 12:18:10 -0800

    enable bulk update

--- dumbhead/management/commands/crawl_bestbuy_price.py
+++ dumbhead/management/commands/crawl_bestbuy_price.py
@@ -7,11 +7,11 @@ from dumbhead.models import Product, PriceHistory
 
 MAXPAGE = 5
 BESTBUY = 'bestbuy'
-HOST = 'https://api.bestbuy.com/v1/products/'
+HOST = 'https://api.bestbuy.com/v1/products(sku in({search}))?'
 KEY = 'mQG1n4CF8F7YfvGtyAoMQdGY'
-PARAM = '{sku}.json?apiKey={key}&show={show}'
+PARAM = 'format=json&apiKey={key}&show={show}'
 SEARCH_URL = HOST + PARAM
-ATTRIBUTES = ['source', 'salePrice']
+ATTRIBUTES = ['source', 'salePrice', 'sku']
 
 
 class Command(BaseCommand):
@@ -21,27 +21,34 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Handle command."""
+        pids = []
         for index, product in enumerate(Product.objects.all()):
-            print('getting====>', product.to_json())
-            self.get_products_price(product.id)
-            if index % 2 == 0:
-                time.sleep(1)
+            if index % 100 == 0 and pids:
+                print('getting====>', pids)
+                self.get_products_price(pids)
+                if index % 200 == 0:
+                    time.sleep(1)
+                pids = []
+            else:
+                pids.append(product.id)
 
-    def get_products_price(self, pid):
+    def get_products_price(self, pids):
         """Get products by category."""
         total_num = MAXPAGE
         num = 1
         while(num <= total_num):
             url = SEARCH_URL.format(
-                key=KEY, show=','.join(ATTRIBUTES), sku=pid)
+                key=KEY, show=','.join(ATTRIBUTES), search=','.join(pids))
             data = requests.get(url).json()
             if 'fault' in data or 'error' in data:
                 print(data)
                 num += 1
                 continue
-            price_his = PriceHistory.objects.filter(
-                website=BESTBUY, pid=pid).first()
-            if not price_his or price_his.salePrice != data['salePrice']:
-                PriceHistory(website=BESTBUY, pid=pid,
-                             salePrice=data['salePrice']).save()
+            for p_data in data['products']:
+                pid = str(p_data['sku'])
+                price_his = PriceHistory.objects.filter(
+                    website=BESTBUY, pid=pid).first()
+                if not price_his or price_his.salePrice != p_data['salePrice']:
+                    PriceHistory(website=BESTBUY, pid=pid,
+                                 salePrice=p_data['salePrice']).save()
             break
